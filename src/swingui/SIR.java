fcd479e10c42951c08c61f38cd0ee1f9021b11b5
Robyn McNamara <ram@panacea.null.org>
2015-02-23 14:53:02 +1100

    Removed Nimbus L&F.  It was a silly idea.

--- src/swingui/SIR.java
+++ src/swingui/SIR.java
@@ -15,18 +15,6 @@ public class SIR {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				
-				// Set Nimbus l&f
-				try {
-				    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-				        if ("Nimbus".equals(info.getName())) {
-				            UIManager.setLookAndFeel(info.getClassName());
-				            break;
-				        }
-				    }
-				} catch (Exception e) {
-				    // If Nimbus is not available, you can set the GUI to another look and feel.
-				}
-				
 				try {
 					SIRMainFrame window = new SIRMainFrame();
 					window.setVisible(true);
