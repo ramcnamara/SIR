496c989d04e57c7587a7041d5f10520ba7acf7ac
Robyn McNamara <ram@panacea.null.org>
2015-02-23 14:28:38 +1100

    Switched L&F to Nimbus

--- src/swingui/SIR.java
+++ src/swingui/SIR.java
@@ -3,6 +3,9 @@ package swingui;
 
 import java.awt.EventQueue;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 
 public class SIR {
 	/**
@@ -11,6 +14,19 @@ public class SIR {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				
+				// Set Nimbus l&f
+				try {
+				    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				        if ("Nimbus".equals(info.getName())) {
+				            UIManager.setLookAndFeel(info.getClassName());
+				            break;
+				        }
+				    }
+				} catch (Exception e) {
+				    // If Nimbus is not available, you can set the GUI to another look and feel.
+				}
+				
 				try {
 					SIRMainFrame window = new SIRMainFrame();
 					window.setVisible(true);
